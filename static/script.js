c20dd965518ab293bff408bb31e4a52e892d1b05
lahg1103 <leslie.hurtado02@utrgv.edu>
2023-10-21 22:19:18 -0500

    googleapi working

--- static/script.js
+++ static/script.js
@@ -16,33 +16,33 @@
 
   window.addEventListener('DOMContentLoaded', () => {
 
-    function random(range, unit) {
-    let randNum = Math.floor(Math.random() * range) + 1;
-    return `$(randNum)$(unit)`;
-    }
-    function createStar(s) {
-            let star = document.createElement('div');
-            star.classList.add('star');
+    // function random(range, unit) {
+    // let randNum = Math.floor(Math.random() * range) + 1;
+    // return `$(randNum)$(unit)`;
+    // }
+    // function createStar(s) {
+    //         let star = document.createElement('div');
+    //         star.classList.add('star');
 
-            let randRange = Math.floor(Math.random() * 5) + 1;
-            star.classList.add(`blink_${randRange}`);
+    //         let randRange = Math.floor(Math.random() * 5) + 1;
+    //         star.classList.add(`blink_${randRange}`);
 
-            let widthAndHeight = random(s, 'px');
-            star.style.height = star.style.width = widthAndHeight;
+    //         let widthAndHeight = random(s, 'px');
+    //         star.style.height = star.style.width = widthAndHeight;
 
-            star.style.left = random(window.innerWidth, 'px');
-            star.style.top = random(window.innerHeight, 'px');
+    //         star.style.left = random(window.innerWidth, 'px');
+    //         star.style.top = random(window.innerHeight, 'px');
 
-            sky.appendChild(star);
-        }
-    function renderStar(stars, size) {
-        while (sky.firstChild) {
-            sky.removeChild(sky.firstChild);
-        }
-        for (let i = 0; i < stars; i++) {
-            createStar(size);
-        }
-    }
+    //         sky.appendChild(star);
+    //     }
+    // function renderStar(stars, size) {
+    //     while (sky.firstChild) {
+    //         sky.removeChild(sky.firstChild);
+    //     }
+    //     for (let i = 0; i < stars; i++) {
+    //         createStar(size);
+    //     }
+    // }
 
     let navHeight = document.querySelector('nav').clientHeight;
 
@@ -51,7 +51,7 @@
     landingContent.style.paddingTop = navHeight * (navHeight * 0.1) + 'px';
     landingContent.style.height = 'calc(100vh - ' + (navHeight + (navHeight * 0.9)) * 2 + 'px)'; 
     
-    var sky = document.querySelector('.sky');
+    // var sky = document.querySelector('.sky');
     
-    renderStar(50, 4);
+    // renderStar(50, 4);
 });
